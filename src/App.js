36543b187bc8e101f342007288cffb33c1205c5d
Maja <85358169+majo1ka@users.noreply.github.com>
2023-07-24 19:18:18 +0200

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
-
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a href="https://github.com/majo1ka/react-weather" target="_blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
