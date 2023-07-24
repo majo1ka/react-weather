d97fd49b18d45e4edeaf4eea3bb763675c69a32d
Maja <85358169+majo1ka@users.noreply.github.com>
2023-07-24 20:54:43 +0200

    CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           <a href="https://github.com/majo1ka/react-weather" target="_blank">
             GitHub
---
+++ src/Weather.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-4">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-dark w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Prague</h1>
+      <ul>
+        <li>Monday</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">23</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation 4%</li>
+            <li>Humidity: 56%</li>
+            <li>Wind: 26 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
